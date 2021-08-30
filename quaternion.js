68893cbc93e71bf4fe8315b526dd52578fdb13f7
Lucas544875 <takayanagi691@gmail.com>
2021-08-31 00:12:53 +0900

    コンストラクタの作成

--- quaternion.js
+++ quaternion.js
@@ -1,3 +1,13 @@
 class Quatarnion {
+  constructor(w,i,j,k){
+    this.w = w;
+    this.i = i;
+    this.j = j;
+    this.k = k;
+  }
 
+  inverce(){
+    return new Quatarnion(this.w,-this.i,-this.j,-this.k);
+  }
+  
 }
