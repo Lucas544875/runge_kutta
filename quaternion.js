5f4353d79fb96a6663f133fccb736c608f4b1584
Lucas544875 <takayanagi691@gmail.com>
2021-09-02 11:37:10 +0900

    デバッガの導入

--- quaternion.js
+++ quaternion.js
@@ -4,10 +4,18 @@ class Quatarnion {
     this.i = i;
     this.j = j;
     this.k = k;
-  }
+  };
 
   inverce(){
     return new Quatarnion(this.w,-this.i,-this.j,-this.k);
+  };
+
+  static vec(x,y,z){
+    return new Quatarnion(0,x,y,z);
+  }
+  
+  static rotate(){
+    return;
   }
   /* 
   欲しい関数
@@ -16,7 +24,7 @@ class Quatarnion {
   vec to qua
   lookat
   cross
-
+  normalize
   v=Quartation.vec(1,2,3)
   q=v.lookat(0,0,0)
 
