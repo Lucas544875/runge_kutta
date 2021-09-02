4862209ccebc08b8510da32f0fc40803caaf90b7
Lucas544875 <takayanagi691@gmail.com>
2021-09-02 10:58:22 +0900

    昨日の進捗

--- quaternion.js
+++ quaternion.js
@@ -9,5 +9,16 @@ class Quatarnion {
   inverce(){
     return new Quatarnion(this.w,-this.i,-this.j,-this.k);
   }
-  
+  /* 
+  欲しい関数
+  q1.times(q2)
+  q2=q1.inverse()
+  vec to qua
+  lookat
+  cross
+
+  v=Quartation.vec(1,2,3)
+  q=v.lookat(0,0,0)
+
+  */
 }
